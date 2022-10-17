b70e170f88d5a03d23ee9ada45773f79d3884d5d
cuong.dm <cuong.dm@seesaa.vn>
2022-10-17 15:53:07 +0700

    config

---
+++ HacoKit/HacoKit.h
@@ -0,0 +1,18 @@
+//
+//  HacoKit.h
+//  HacoKit
+//
+//  Created by SeesaaVN on 10/17/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HacoKit.
+FOUNDATION_EXPORT double HacoKitVersionNumber;
+
+//! Project version string for HacoKit.
+FOUNDATION_EXPORT const unsigned char HacoKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HacoKit/PublicHeader.h>
+
+
